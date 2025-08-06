58fb4273e0f5be5f34c44ea84e7a03456abfae75
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-08-07 02:43:58 +0530

    Update language.py

--- MoonXMusic/utils/decorators/language.py
+++ MoonXMusic/utils/decorators/language.py
@@ -1,14 +1,16 @@
 from MoonXMusic.misc import SUDOERS
 from MoonXMusic.utils.database import get_lang, is_maintenance
 from strings import get_string
+from config import SUPPORT_CHAT
+from MoonXMusic import app
 
 
 def language(mystic):
     async def wrapper(_, message, **kwargs):
-        if await is_maintenance() is False:
+        if not await is_maintenance():
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    text=f"{app.mention} is under maintenance. Please visit <a href='{SUPPORT_CHAT}'>support chat</a> for updates.",
                     disable_web_page_preview=True,
                 )
         try:
@@ -28,10 +30,10 @@ def language(mystic):
 
 def languageCB(mystic):
     async def wrapper(_, CallbackQuery, **kwargs):
-        if await is_maintenance() is False:
+        if not await is_maintenance():
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    f"{app.mention} is under maintenance. Please check support chat.",
                     show_alert=True,
                 )
         try:
@@ -54,4 +56,3 @@ def LanguageStart(mystic):
         return await mystic(_, message, language)
 
     return wrapper
-        
