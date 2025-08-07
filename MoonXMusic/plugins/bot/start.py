e0861780545b3d5fd3095d30ffa9072d7af46a5b
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-08-07 16:09:07 +0530

    Update start.py

--- MoonXMusic/plugins/bot/start.py
+++ MoonXMusic/plugins/bot/start.py
@@ -120,7 +120,7 @@ async def start_pm(client, message: Message, _):
                         text=_["S_B_6"], url=config.SUPPORT_CHANNEL,
                     ),
                     InlineKeyboardButton(
-                        text="_[S_B_7"], url=config.START_IMG_URL,
+                        text="_["S_B_7"], url=config.START_IMG_URL,
                     )
                 ],
                 ]
