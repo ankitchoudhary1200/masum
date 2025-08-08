d7f5640d9628ea40d4a98a9bdee6c14aecc5cae7
ruchi5134 <kn043613@gmail.com>
2025-08-08 10:01:10 +0530

    Update userbot.py

--- MoonXMusic/core/userbot.py
+++ MoonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("masoomupdate")
-                await self.one.join_chat("masoomupdate")
+                await self.one.join_chat("alpna_updates")
+                await self.one.join_chat("alpna_updates")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("masoomupdate")
-                await self.one.join_chat("masoomupdate")
+                await self.two.join_chat("alpna_updates")
+                await self.one.join_chat("alpna_updates")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("masoomupdate")
-                await self.one.join_chat("masoomupdate")
+                await self.three.join_chat("alpna_updates")
+                await self.one.join_chat("alpna_updates")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("world_friend_chatting_zone")
-                await self.one.join_chat("world_friend_chatting_zone")
+                await self.four.join_chat("alpna_updates")
+                await self.one.join_chat("alpna_updates")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("masoomupdate")
-                await self.one.join_chat("masoomupdate")
+                await self.five.join_chat("alpna_updates")
+                await self.one.join_chat("alpna_updates")
             except:
                 pass
             assistants.append(5)
