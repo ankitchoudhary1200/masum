28de2f8b5ebe48ee66ee455ba17938e0265171c5
ankitchoudhary1200 <ankitchoudhary125677@gmail.com>
2025-09-09 21:02:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-https://github.com/ankitchoudhary1200/masum)
+https://github.com/ankitchoudhary1200/masum
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
